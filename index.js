69272d5aafe73762a9c1707af2764f1888002364
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-05-12 23:32:49 -0700

    disabled draw btn while cards remaining is at 0 or before user clicks new deck i also added a reset function whenever new deck is clicked

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 let deckId
 const cardsContainer = document.getElementById("cards")
-document.getElementById("draw-cards").addEventListener("click", drawCards)
-document.getElementById("new-deck").addEventListener("click", handleClick)
+const drawCards = document.getElementById("draw-cards")
+const newDeckBtn = document.getElementById("new-deck")
 const result = document.getElementById("result")
 const cardsRemaining = document.getElementById("cards-remaining")
 const wholeDeck = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"]
 
+newDeckBtn.addEventListener("click", handleClick)
+drawCards.addEventListener("click", draw)
+
+drawCards.disabled = true;
+
 function handleClick() {
+    reset()
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
@@ -14,20 +20,26 @@ function handleClick() {
             deckId = data.deck_id
             
             cardsRemaining.innerHTML = `Cards remaining: ${data.remaining}`
+            drawCards.removeAttribute("disabled")
+
         })
 }
 
-function drawCards(){
+function draw(){
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
             console.log(data.cards)
             cardsRemaining.innerHTML = `Cards remaining: ${data.remaining}`
-            for(let i = 0; i < cardsContainer.children.length; i++){
-                cardsContainer.children[i].innerHTML = `<img src=${data.cards[i].image} class="card">`
-            }
+            
+            cardsContainer.children[0].innerHTML = `<img src=${data.cards[0].image} class="card">`
+            cardsContainer.children[1].innerHTML = `<img src=${data.cards[1].image} class="card">`
+
             const winner = declareWinner(data.cards[0], data.cards[1])
             result.innerHTML = winner
+            if(data.remaining <= 0){
+                drawCards.disabled = true
+            }
         })
 }
 
@@ -42,4 +54,10 @@ function declareWinner(cardOne, cardTwo){
     } else {
        return "Draw"
     }
+}
+
+function reset(){
+    cardsContainer.children[0].innerHTML = ""
+    cardsContainer.children[1].innerHTML = ""
+    result.innerHTML = "Game of War"
 }
