4aa1fa57c6fa65a1dd96ce16948b6ac321aa4ec9
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-05-21 23:57:16 -0700

    added button to draw cards and display them on the html

--- index.js
+++ index.js
@@ -9,4 +9,17 @@ function handleClick() {
         })
 }
 
+function drawCards(){
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.cards)
+            document.getElementById("cards").innerHTML = `
+                <img src=${data.cards[0].image} />
+                <img src=${data.cards[1].image} />
+            `
+        })
+}
+
+document.getElementById("draw-cards").addEventListener("click", drawCards)
 document.getElementById("new-deck").addEventListener("click", handleClick)
